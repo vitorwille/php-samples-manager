bb60a6503dc1ade23bbdbec1a5bb8aeccbbec642
Vitor Wille <vitorviciado296@gmail.com>
2026-08-18 14:55:39 -0300

    feat: add título dinâmico nas páginas

---
+++ frontend/src/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'MicroLIMS - Amostras',
+};
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return children;
+}
---
+++ frontend/src/app/login/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'MicroLIMS - Acesso',
+};
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+    return children;
+}
